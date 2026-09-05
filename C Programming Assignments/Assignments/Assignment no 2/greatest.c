2a7817820e895038ceb05b7a7a3192bba8bf6ee7
Shelke Rushikesh <shelkerushikesh2005@gmail.com>
2026-09-05 22:44:13 +0530

    Add files via upload

---
+++ C Programming Assignments/Assignments/Assignment no 2/greatest.c
@@ -0,0 +1,13 @@
+void main()
+{
+	int a,b,c;
+	printf("Enter the number");
+	scanf("%d%d%d",&a,&b,&c);
+	
+	if(a>b)
+		printf("a is greater");
+	else if(b>c)
+		printf("b is greater");
+	else
+		printf("c is greater");
+}
